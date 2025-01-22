4517db1eb6773284ce6ea0d30bf329167f92182a
yarbest <yarbest15@gmail.com>
2025-01-22 18:52:14 +0200

    changes urls on client and server

--- client/src/routes/paths.ts
+++ client/src/routes/paths.ts
@@ -1,6 +1,6 @@
 export const paths = {
-  root: `${import.meta.env.PROD ? '/vite-test' : ''}/`,
-  users: `${import.meta.env.PROD ? '/vite-test' : ''}/users`,
-  notFound: `${import.meta.env.PROD ? '/vite-test' : ''}/not-found`,
-  login: `${import.meta.env.PROD ? '/vite-test' : ''}/login`,
+  root: `${import.meta.env.PROD ? '/fullstack-authentication' : ''}/`,
+  users: `${import.meta.env.PROD ? '/fullstack-authentication' : ''}/users`,
+  notFound: `${import.meta.env.PROD ? '/fullstack-authentication' : ''}/not-found`,
+  login: `${import.meta.env.PROD ? '/fullstack-authentication' : ''}/login`,
 }
