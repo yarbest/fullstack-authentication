af049fc959a8d2a170f9044adb0ac15b071fe329
yarbest <yarbest15@gmail.com>
2025-02-04 13:41:29 +0200

    Revert "Update App.tsx"
    This reverts commit 77f51d6710973312f955218c7800431bb41d74fc.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { ErrorBoundary, FallbackProps } from 'react-error-boundary'
 import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import Router from './routes/Router'
 import { store } from './store'
@@ -17,13 +17,13 @@ function fallbackRender({ error }: FallbackProps) {
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Provider store={store}>
         <ErrorBoundary fallbackRender={fallbackRender}>
           <Router />
         </ErrorBoundary>
       </Provider>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
 
